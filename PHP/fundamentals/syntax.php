06c042b7857750be3ee0f797be3cabb02e178460
Ashikur_Rahman <ashikurrahman7134@gmail.com>
2025-02-18 14:57:36 +0600

    file structure and php basics syntax

---
+++ PHP/fundamentals/syntax.php
@@ -0,0 +1,26 @@
+<?php 
+    echo "Hello World";
+?>
+
+<?= "Short echo tag"; ?>
+
+<?php for($i=0; $i <5; $i++) : ?>
+    <h1> Hello, there! </h1>
+<?php endfor ?>
+
+<?php 
+
+// THis is comment
+$name = "Ashik"; 
+echo $name; // output: Ashik
+/* 
+    This is 
+    multiline comment.
+*/
+
+/*
+    * This is detailed explanation
+    * of something that should required
+    * several paragraph of information
+*/
+?>
