9e3b6814dca361edf27355418b0a65bb8bea6d08
Ashikur_Rahman <ashikurrahman7134@gmail.com>
2025-03-19 00:38:08 +0600

    DOM Practice

--- JS/basics/date.js
+++
@@ -1,6 +0,0 @@
-let date = new Date()
-console.log(date.toDateString())
-console.log(date.toTimeString())
-console.log(date.toLocaleString())
-console.log(date.toLocaleDateString())
-console.log(date.toLocaleTimeString())
