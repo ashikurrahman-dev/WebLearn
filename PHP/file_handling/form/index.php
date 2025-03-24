3a989d807f67b9125f3f60998f32f2a7bc026f93
Ashikur_Rahman <ashikurrahman7134@gmail.com>
2025-03-24 11:25:08 +0600

    form handling

---
+++ PHP/file_handling/form/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<br>
+    <form action="action.php" method="post">
+        Name: <input type="text" name="name" id=""></br></br>
+        Age: <input type="text" name="age"></br></br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
